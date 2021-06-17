2e6f375de35c1cb31053db9b6c5c359b81940735
Daniel-Carpenter <dbry1024@gmail.com>
2021-06-17 09:36:45 -0500

    Single file to source other code reuses

---
+++ R/Code_Reuse/- Main Source Files.R
@@ -0,0 +1,9 @@
+# Library
+source("C:/Users/daniel.carpenter/OneDrive - the Chickasaw Nation/Documents/GitHub/Coding-Resources/R/Code_Reuse/Library.R")
+
+# DBC Theme
+source("C:/Users/daniel.carpenter/OneDrive - the Chickasaw Nation/Documents/GitHub/Coding-Resources/R/Code_Reuse/DBC_Theme.R")
+
+# OPA Database
+source("C:/Users/daniel.carpenter/OneDrive - the Chickasaw Nation/Documents/GitHub/Coding-Resources/R/Code_Reuse/OPA_Database_Connection.R")
+
