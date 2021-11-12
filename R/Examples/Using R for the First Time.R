a40d1aba383c846dde174417531f90dcb6fb2836
Daniel Carpenter <59896063+Daniel-Carpenter@users.noreply.github.com>
2021-11-12 10:13:01 -0600

    Setup file

---
+++ R/Examples/Using R for the First Time.R
@@ -0,0 +1,14 @@
+
+# Overview ===================================================
+  # 1. Library
+  # 2. Read in file
+  # 3. Summary stats about the file's columns  (summary, or skim)
+  # 4. Filter data, if statements, basic operators, etc. 
+  # 5. Alter data (select, mutate, group by, pivot?)
+  # 6. Join on another dataset
+  # 7. Create a basic visual
+  # 9. Output data to file (sort/present)
+
+# Extras =====================================================
+  # For Loops
+  
