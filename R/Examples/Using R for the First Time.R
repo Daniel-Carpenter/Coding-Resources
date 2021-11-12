363345b229c772d563c9db603de021d9835e0597
Daniel Carpenter <59896063+Daniel-Carpenter@users.noreply.github.com>
2021-11-12 10:34:08 -0600

    Read in excel and skim data frame

--- R/Examples/Using R for the First Time.R
+++ R/Examples/Using R for the First Time.R
@@ -1,9 +1,20 @@
+# Comments here - Example of using R for First time!
 
 # Overview ===================================================
+  
   # 1. Library
+    library(tidyverse)
+    library(readxl) # read_excel function
+    library(skimr) # skim function to see summary stats of a data frame (df)
+
   # 2. Read in file
-  # 3. Summary stats about the file's columns  (summary, or skim)
-  # 4. Filter data, if statements, basic operators, etc. 
+    suppressWarnings(df <- read_excel("data.xlsx"))
+
+  # 3. Summary stats about the file's columns  
+    skim(df)
+    # summary(df) # this is an alternative
+    
+  # 4. Filter data, if statements, basic operators, etc. as
   # 5. Alter data (select, mutate, group by, pivot?)
   # 6. Join on another dataset
   # 7. Create a basic visual
