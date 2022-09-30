0246d9c8cb3ac60b834d41d15249237c796d11f6
Daniel-Carpenter <dbry1024@gmail.com>
2022-09-30 08:23:21 -0500

    add more ntoes

--- SQL/Examples/Grant Permissions.sql
+++ SQL/Examples/Grant Permissions.sql
@@ -8,3 +8,9 @@
 -- GRANT CREATE TABLE TO "INT\User.Name";
 
 -- GRANT CREATE VIEW TO "INT\User.Name";
+
+
+-- Select only with create views to new database
+-- USE DatabaseName; 
+-- GRANT CONNECT, SELECT, CREATE VIEW, VIEW DEFINITION TO "INT\User.Name"
+-- EXEC sp_addrolemember N'db_datareader', N'INT\User.Name'
