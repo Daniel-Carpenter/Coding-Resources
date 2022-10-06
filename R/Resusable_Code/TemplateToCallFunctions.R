1c2b8233cbdc203aeed85de88756ccba4a4fccda
Daniel-Carpenter <dbry1024@gmail.com>
2022-10-06 16:42:34 -0500

    Generalize

--- R/Resusable_Code/TemplateToCallFunctions.R
+++ R/Resusable_Code/TemplateToCallFunctions.R
@@ -2,17 +2,17 @@
 # ==============================================================================
 # functionNameHere()
 # Function that <does something>
-# Source Data: OPA.Schema.Table
+# Source Data: Database.Schema.Table
 # Returns: <a table with a description>
-# How to Run: Make sure to open R project, then run file in 'Data_Pipeline' folder
+# How to Run: Make sure to open R project, then run file
 # ==============================================================================
 
 
 # Source Reused functions
-locationOfFunctions = 'Reusable_Functions//'
-source(paste0(locationOfFunctions, 'Library',                 '.R')) # Library (tidyverse, lubridate, etc.)
-source(paste0(locationOfFunctions, 'OPA_Database_Connection', '.R')) # Connect to OPA database
-source(paste0(locationOfFunctions, 'OPA_Theme',               '.R')) # Get the OPA theme
+locationOfFunctions = 'Reusable_Code//'
+source(paste0(locationOfFunctions, 'Library',             '.R')) # Library (tidyverse, lubridate, etc.)
+source(paste0(locationOfFunctions, 'Database_Connection', '.R')) # Connect to a database
+source(paste0(locationOfFunctions, 'DBC-Styles',          '.R')) # Get the DBC style and theme
 
 
 # ==============================================================================
