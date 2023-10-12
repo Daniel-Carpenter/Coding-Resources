40e83ed4096dba54fce5a1280fda40f339d4c374
Daniel Carpenter <dbry1024@gmail.com>
2023-10-12 09:43:34 -0500

    Blank Python Example Script

---
+++ Python/03 - Examples/Python-Syntax.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Goal: Python Syntax Tutorial
+
+Created on Thu Oct 12 09:20:05 2023
+
+@author: Daniel Carpenter
+
+Resources: https://www.w3schools.com/python/default.asp
+"""
+
+
+
+# =============================================================================
+# (1) Object Assignment/Creation
+# =============================================================================
+
+## (1.1) Single-Valued Variables
+
+## (1.2) *Multiple* Single-Valued Variables
+
+## (1.3) Lists
+
+
+
+# =============================================================================
+# (2) Basic Arithmetic Operations
+# 
+# Resources:
+#     https://www.w3schools.com/python/python_operators.asp
+# =============================================================================
+
+## (2.1) Single-Valued Variables (PEMDAS)
+
+## (2.3) Lists
+
+## (2.4) Scaler Variables (Boolean [True/False])
+
+
+# =============================================================================
+# (3) if/else/elif
+# =============================================================================
+
+## (3.1) Basic if / else / else if
+
+## (3.2) Multiple Conditions with simple if
+
+
+
+# =============================================================================
+# (4) Functions
+# =============================================================================
+
+## (4.1) Print Function
+
+## (4.2) Function that Returns Something
+
+
+
+# =============================================================================
+# (5) Loops
+# =============================================================================
+
+## (5.1) While
+
+## (5.2) For Loop
+
+## (5.3) *Nested* For Loop
+
+
+
+# =============================================================================
+# (6) Appendix (further reading)
+# =============================================================================
+
+# Arithmetic Operators
+# Tuple: https://www.w3schools.com/python/python_tuples.asp
+# Sets: https://www.w3schools.com/python/python_sets.asp
+# Dictionaries: https://www.w3schools.com/python/python_dictionaries.asp
+
+# Data types
+# How to convert data types: https://www.w3schools.com/python/python_casting.asp
+
