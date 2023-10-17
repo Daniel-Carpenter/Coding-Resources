831f85d584d6cf7a5a4e51fe20ad0dd7760ee645
Daniel Carpenter <dbry1024@gmail.com>
2023-10-17 14:49:28 -0500

    Work through pandas example

--- Python/03 - Examples/Pandas-Intro.py
+++ Python/03 - Examples/Pandas-Intro.py
@@ -6,12 +6,14 @@
 @author: Daniel Carpenter
 
 @resources: 
+    Spyder Proj: https://docs.spyder-ide.org/current/panes/projects.html
     Data prep: https://www.mit.edu/~amidi/teaching/data-science-tools/study-guide/data-manipulation-with-python/
     Visuals:   https://www.mit.edu/~amidi/teaching/data-science-tools/study-guide/data-visualization-with-python/
     Seaborn:   https://seaborn.pydata.org/generated/seaborn.boxplot.html
     Pandas:    https://pandas.pydata.org/docs/reference/frame.html
 """
 
+import pandas as pd # import the pandas library
 
 
 # =============================================================================
@@ -19,8 +21,10 @@
 # =============================================================================
 
 ## Read CSV File
+df_base = pd.read_csv('Data/Life_Expectency.csv')
 
 ## Look at the data (summary stats/viewer)
+# df_base.describe()
 
 
 
@@ -29,27 +33,39 @@
 # =============================================================================
 
 ## Remove Uneccessary Variables
-
-## Rename Variables
+df = df_base.drop(columns = ['SourceName', 'DateAccessed'])
 
 ## Reorganize data into few columns
+numericColumns = df.select_dtypes(include=['number']).columns
+numericColumns = numericColumns[1:]
+
+df = df.melt(
+    
+    id_vars    = df.columns[:2], # Which data do I keep?
+    value_vars = numericColumns, # Which columns to put into single values columns (unpivoted)?
+             
+    # Put the data into two columns
+    value_name = 'lifeExpectency',
+    var_name   = 'country'
+    )
+
+
+## Rename Variables
+df = df.rename(columns={'Period': 'yearNum', 
+                        'Dim1':   'gender'
+                        })
 
 ## Filter data to "Both Sexes"
+df = df.query("gender == 'Both sexes'")
 
 ## Create a new calculated variable based on a self made function
-
-## An aside: create the calculated variable on the original "wide" data set using apply
+def calcRelativeValue(nominalValue):
+    relativeValue = nominalValue / max(nominalValue) # Create a relative value (relative to the max value in the column of the data frame)
     
-## Attach the continent name from the country
+    return relativeValue 
 
+df['relativeValue'] = calcRelativeValue(df['lifeExpectency'])
 
-
-# =============================================================================
-# (3) Basic Visual of Data
-# =============================================================================
-
-## Create a scatter by continent
-
-## Create a boxplot by country
-
-
+## Might be good to know: Joining two datasets together:
+# https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.merge.html
+    
